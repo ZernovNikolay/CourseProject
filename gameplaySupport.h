3b3ebf61fbcfb11f962c191f253d2905fcd7a1d7
ZernovNikolay <55160469+ZernovNikolay@users.noreply.github.com>
2020-05-22 04:30:48 +0300

    Comment Update

--- gameplaySupport.h
+++ gameplaySupport.h
@@ -4,24 +4,24 @@
 #include "person.h"
 #include "room.h"
 
-bool CheckWall(const Wall* wall, float x, float y);
+bool CheckWall(const Wall* wall, float x, float y); //проверка конкретной стены
 
-bool CheckWalls(const std::vector<Wall*> walls, float x, float y);
+bool CheckWalls(const std::vector<Wall*> walls, float x, float y); //проверить, не мешают ли движению героя стены
 
-void CheckGiveItems(bool& flag_keyboardE, Person& Our_Hero, Room*& current_room);
+void CheckGiveItems(bool& flag_keyboardE, Person& Our_Hero, Room*& current_room);//поднять предмет, если можно
 
-int CheckItems(const std::vector<std::shared_ptr<Object>>& gh, const Person& Our_Hero);
+int CheckItems(const std::vector<std::shared_ptr<Object>>& gh, const Person& Our_Hero); //проверить, можно ли поднять предмет
 
-bool CheckItem(const std::shared_ptr<Object>& item, const Person& Our_Hero);
+bool CheckItem(const std::shared_ptr<Object>& item, const Person& Our_Hero); //проверить конкретный предмет
 
-int CheckDoors(Room* current_room, const Person& Our_Hero);
+int CheckDoors(Room* current_room, const Person& Our_Hero); //проверить можно ли использовать дверь
 
-bool CheckDoor(Door* door, const Person& Our_Hero);
+bool CheckDoor(Door* door, const Person& Our_Hero); //проверить конкретную дверь
 
-bool CheckClosedDoor(Door* door, Inventory* sd);
+bool CheckClosedDoor(Door* door, Inventory* sd); //проверить, открыта ли дверь
 
-void CheckButtonLeftOnInventory(bool& flagLeft, Person& Our_Hero, const sf::Vector2i& pixelPos);
+void CheckButtonLeftOnInventory(bool& flagLeft, Person& Our_Hero, const sf::Vector2i& pixelPos); //использовать левую кнопку мыши в инвентаре
 
-void CheckButtonRightOnInventory(bool& flagRight, Person& Our_Hero, const sf::Vector2i& pixelPos, Room*& current_room);
+void CheckButtonRightOnInventory(bool& flagRight, Person& Our_Hero, const sf::Vector2i& pixelPos, Room*& current_room); //использовать правую кнопку мыши в инвентаре
 
 #endif /* GAMEPLAYSUPPORT_H_ */
