aa98cb2e75953fdd9770536d780530ecfe02e0de
ZernovNikolay <55160469+ZernovNikolay@users.noreply.github.com>
2020-05-22 04:27:42 +0300

    Finish Update

--- gameplay.h
+++ gameplay.h
@@ -3,14 +3,12 @@
 
 #include "gameplaySupport.h"
 
-void CheckMoveHero(Person& Our_Hero, Room* current_room);
+void CheckMoveHero(Person& Our_Hero, Room* current_room); //движение героя, если это возможно
 
-void CheckPassDoors(bool& flag_keyboardQ, Person& Our_Hero, Room*& current_room, sf::RenderWindow& window_H);
+void CheckPassDoors(bool& flag_keyboardQ, Person& Our_Hero, Room*& current_room, sf::RenderWindow& window_H); //войти в дверь если это возможно
 
-//void CheckGiveItems(bool& flag_keyboardE, Person& Our_Hero, Room*& current_room);
+void WorkWithInventory(bool& flag_keyboardR, Person& Our_Hero, Room*& current_room); //взаимодействовать с инвентарём
 
-void WorkWithInventory(bool& flag_keyboardR, Person& Our_Hero, Room*& current_room);
-
-void ShowInventory(Person& Our_Hero, Room* current_room);
+void ShowInventory(Person& Our_Hero, Room* current_room);//показать инвентарь
 
 #endif /* GAMEPLAY_H_ */
